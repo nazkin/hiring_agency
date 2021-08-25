ed02703b8f359f09f91b0826e442d173f43deab1
Naz Kin <nazkin@pop-os.localdomain>
2021-08-24 21:06:45 -0400

    small changes

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,9 @@
 M.AutoInit();
 document.addEventListener("DOMContentLoaded", function () {
   var elems = document.querySelectorAll(".sidenav");
-  var instances = M.Sidenav.init(elems, options);
-});
-document.addEventListener("DOMContentLoaded", function () {
-  var elems = document.querySelectorAll(".parallax");
-  var instances = M.Parallax.init(elems, options);
+  var elems2 = document.querySelectorAll(".parallax");
+  var instancesSidenav = M.Sidenav.init(elems, options);
+  var instancesParallex = M.Parallax.init(elems2, options);
+  var elems3 = document.querySelectorAll(".collapsible");
+  var instancesCollapsible = M.Collapsible.init(elems3, options);
 });
