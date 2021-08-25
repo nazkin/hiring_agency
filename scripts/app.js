26d74cb050b71e353d8b1d911bfcef815ed4efa0
Naz Kin <nazkin@pop-os.localdomain>
2021-08-25 16:19:21 -0400

    added animations

--- scripts/app.js
+++ scripts/app.js
@@ -7,3 +7,20 @@ document.addEventListener("DOMContentLoaded", function () {
   var elems3 = document.querySelectorAll(".collapsible");
   var instancesCollapsible = M.Collapsible.init(elems3, options);
 });
+
+// Intersection Observers
+const observer = new IntersectionObserver((entries) => {
+  // Loop over the entries
+  entries.forEach((entry) => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+      entry.target.classList.remove("animate__slideOutRight");
+      entry.target.classList.add("animate__slideInRight");
+      return;
+    }
+  });
+});
+
+//Mission body
+observer.observe(document.querySelector(".mission-txt-container"));
