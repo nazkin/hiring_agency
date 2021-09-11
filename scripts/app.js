f10582455d09d400d87d5115db75421992882be8
Naz Kin <nazkin@pop-os.localdomain>
2021-09-11 12:37:31 -0400

    updates

--- scripts/app.js
+++ scripts/app.js
@@ -7,5 +7,12 @@ document.addEventListener("DOMContentLoaded", function () {
   var elems3 = document.querySelectorAll(".collapsible");
   var instancesCollapsible = M.Collapsible.init(elems3, options);
   var elems4 = document.querySelector(".materialize-textarea");
+
   elems4.characterCounter();
+
+  var elems5 = document.querySelectorAll(".modal");
+  var instances = M.Modal.init(elems5, options);
+  // var elems6 = document.querySelectorAll("select");
+  // var instancesSelect = M.FormSelect.init(elems6, options);
 });
+document.addEventListener("DOMContentLoaded", function () {});
--- scripts/services.js
+++ scripts/services.js
@@ -2,28 +2,57 @@
 const observer = new IntersectionObserver((entries) => {
   // Loop over the entries
   entries.forEach((entry) => {
+    console.log(entry);
     // If the element is visible
     if (entry.isIntersecting) {
-      if (entry.target.attributes[0].value === "serv-checklist") {
+      if (entry.target.attributes[0].value === "service-text-1") {
+        // entry.target.classList.remove("animate__slideOutLeft");
+        entry.target.classList.add("animate__fadeInLeftBig");
+        return;
+      }
+      if (entry.target.attributes[0].value === "service-text-2") {
+        // entry.target.classList.remove("animate__slideOutLeft");
+        entry.target.classList.add("animate__fadeInRightBig");
+        return;
+      }
+      if (entry.target.attributes[0].value === "role-row-1") {
         // entry.target.classList.remove("animate__slideOutLeft");
         entry.target.classList.add("animate__fadeInLeft");
+        entry.target.classList.add("animate__delay-1s");
         return;
       }
-      if (entry.target.attributes[0].value === "checklist-img") {
+      if (entry.target.attributes[0].value === "role-row-2") {
         // entry.target.classList.remove("animate__slideOutLeft");
         entry.target.classList.add("animate__fadeInRight");
+        entry.target.classList.add("animate__delay-1s");
         return;
       }
-      if (entry.target.attributes[0].value === "business_process") {
+      if (entry.target.attributes[0].value === "area-title") {
         // entry.target.classList.remove("animate__slideOutLeft");
-        entry.target.classList.add("animate__fadeInUp");
+        entry.target.classList.add("animate__fadeInRight");
+        entry.target.classList.add("animate__delay-1s");
+        return;
+      }
+      if (entry.target.attributes[0].value === "area-cities") {
+        // entry.target.classList.remove("animate__slideOutLeft");
+        entry.target.classList.add("animate__fadeInLeft");
+        entry.target.classList.add("animate__delay-1s");
+        return;
+      }
+      if (entry.target.attributes[0].value === "service-areas") {
+        // entry.target.classList.remove("animate__slideOutLeft");
+        entry.target.classList.add("animate__fadeIn");
         entry.target.classList.add("animate__delay-1s");
         return;
       }
     }
   });
 });
-observer.observe(document.querySelector("#serv-checklist"));
-observer.observe(document.querySelector("#checklist-img"));
 
-observer.observe(document.querySelector("#business_process"));
+observer.observe(document.querySelector("#service-text-1"));
+observer.observe(document.querySelector("#service-text-2"));
+observer.observe(document.querySelector("#role-row-1"));
+observer.observe(document.querySelector("#role-row-2"));
+observer.observe(document.querySelector("#area-title"));
+observer.observe(document.querySelector("#area-cities"));
+observer.observe(document.querySelector("#service-areas"));
