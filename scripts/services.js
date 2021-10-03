9031c45212d43f55431efae0762df0a6d87e425b
Naz Kin <nazkin@pop-os.localdomain>
2021-10-03 19:07:12 -0400

    form data

--- scripts/services.js
+++ scripts/services.js
@@ -1,13 +1,47 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var parallax = document.querySelectorAll(".parallax");
-  var parallaxInstance = M.Parallax.init(parallax, options);
+//FORM
+//Contact form selectors
+const businessName = document.querySelector("#business_name");
+const contactName = document.querySelector("#contact_name");
+const phone = document.querySelector("#contact_phone");
+const email = document.querySelector("#contact_email");
+const date = document.querySelector("#date");
+const fullTime = document.querySelector("#full");
+const partTime = document.querySelector("#part");
+const bothTimes = document.querySelector("#both");
+const submitBtn = document.querySelector("#submit-form");
+const errorMessage = document.querySelector("#err");
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const businessValue = businessName.value;
+  const contactValue = contactName.value;
+  const phoneValue = phone.value;
+  const emailValue = email.value;
+  const dateValue = date.value;
+  let typeOfJob = "";
+  if (fullTime.checked) {
+    typeOfJob = fullTime.value;
+  } else if (partTime.checked) {
+    typeOfJob = partTime.value;
+  } else if (bothTimes.checked) {
+    typeOfJob = "Both part time and full time could work";
+  }
+
+  const formObject = {
+    business_name: businessValue,
+    contact_name: contactValue,
+    phone: phoneValue,
+    email: emailValue,
+    date: dateValue,
+    jobType: typeOfJob,
+  };
+  console.log(formObject);
 });
 
 // Intersection Observers
 const observer = new IntersectionObserver((entries) => {
   // Loop over the entries
   entries.forEach((entry) => {
-    console.log(entry);
     // If the element is visible
     if (entry.isIntersecting) {
       if (entry.target.attributes[0].value === "service-text-1") {
@@ -61,35 +95,3 @@ observer.observe(document.querySelector("#role-row-2"));
 observer.observe(document.querySelector("#area-title"));
 observer.observe(document.querySelector("#area-cities"));
 observer.observe(document.querySelector("#service-areas"));
-
-//FORM
-//Contact form selectors
-const businessName = document.querySelector("#business_name");
-const contactName = document.querySelector("#contact_name");
-const phone = document.querySelector("#contact_phone");
-const email = document.querySelector("#contact_email");
-const date = document.querySelector("#date");
-const fullTime = document.querySelector("#full");
-const partTime = document.querySelector("#part");
-const bothTimes = document.querySelector("#both");
-const submitBtn = document.querySelector("#submit-btn");
-const errorMessage = document.querySelector("#err");
-
-submitBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  const businessValue = businessName.value;
-  const contactValue = contactName.value;
-  const phoneValue = phone.value;
-  const emailValue = email.value;
-  const dateValue = date.value;
-  const typeOfJob = "";
-
-  const formObject = {
-    business_name: businessValue,
-    contact_name: contactValue,
-    phone: phoneValue,
-    email: emailValue,
-    date: dateValue,
-  };
-  console.log(formObject);
-});
