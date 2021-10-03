81c65d6ba87151eb8c40ebdcd05fd644e3743b2b
Naz Kin <nazkin@pop-os.localdomain>
2021-10-03 18:40:20 -0400

    forms and footer

---
+++ scripts/contact.js
@@ -0,0 +1,46 @@
+//Contact form selectors
+const full_name = document.querySelector("#full_name");
+const email = document.querySelector("#email");
+const phone = document.querySelector("#phone");
+const role = document.querySelector("#role");
+const message = document.querySelector("#message");
+const submitBtn = document.querySelector("#submit");
+const errorMessage = document.querySelector("#err");
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const nameValue = full_name.value;
+  const emailValue = email.value;
+  const phoneValue = phone.value;
+  const roles = [];
+  for (let i = 1; i < role.selectedOptions.length; i++) {
+    roles.push(role.selectedOptions[i].innerText);
+  }
+  const rolesValue = roles.join(" -&- ");
+  const messageValue = message.value;
+
+  //_______________________________________________Basic validation
+  if (!full_name.value) {
+    errorMessage.classList.remove("form-err");
+    errorMessage.classList.add("form-err-active");
+    return;
+  } else if (!email.value || !phone.value) {
+    errorMessage.classList.remove("form-err");
+    errorMessage.classList.add("form-err-active");
+    return;
+  } else if (!rolesValue) {
+    errorMessage.classList.remove("form-err");
+    errorMessage.classList.add("form-err-active");
+    return;
+  }
+  //________________________________________________
+
+  const formObject = {
+    name: nameValue,
+    email: emailValue,
+    phone: phoneValue,
+    roles: rolesValue,
+    message: messageValue,
+  };
+  console.log(formObject);
+});
--- scripts/services.js
+++ scripts/services.js
@@ -61,3 +61,35 @@ observer.observe(document.querySelector("#role-row-2"));
 observer.observe(document.querySelector("#area-title"));
 observer.observe(document.querySelector("#area-cities"));
 observer.observe(document.querySelector("#service-areas"));
+
+//FORM
+//Contact form selectors
+const businessName = document.querySelector("#business_name");
+const contactName = document.querySelector("#contact_name");
+const phone = document.querySelector("#contact_phone");
+const email = document.querySelector("#contact_email");
+const date = document.querySelector("#date");
+const fullTime = document.querySelector("#full");
+const partTime = document.querySelector("#part");
+const bothTimes = document.querySelector("#both");
+const submitBtn = document.querySelector("#submit-btn");
+const errorMessage = document.querySelector("#err");
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const businessValue = businessName.value;
+  const contactValue = contactName.value;
+  const phoneValue = phone.value;
+  const emailValue = email.value;
+  const dateValue = date.value;
+  const typeOfJob = "";
+
+  const formObject = {
+    business_name: businessValue,
+    contact_name: contactValue,
+    phone: phoneValue,
+    email: emailValue,
+    date: dateValue,
+  };
+  console.log(formObject);
+});
